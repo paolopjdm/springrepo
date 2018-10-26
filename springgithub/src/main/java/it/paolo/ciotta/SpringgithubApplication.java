3528f18c18a7d56101d811bdc3bd3370e785ee5d
Stark94 <ciotta.pa@hotmail.it>
2018-10-26 12:21:50 +0200

    first commit

---
+++ springgithub/src/main/java/it/paolo/ciotta/SpringgithubApplication.java
@@ -0,0 +1,12 @@
+package it.paolo.ciotta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringgithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringgithubApplication.class, args);
+	}
+}

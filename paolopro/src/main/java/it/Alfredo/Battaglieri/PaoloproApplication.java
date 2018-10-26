93b5611c37b2063f863810b6ec572b27f257b403
alfredogithub <alfy.battaglieri@gmail.com>
2018-10-26 12:41:10 +0200

    modify

---
+++ paolopro/src/main/java/it/Alfredo/Battaglieri/PaoloproApplication.java
@@ -0,0 +1,12 @@
+package it.Alfredo.Battaglieri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaoloproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaoloproApplication.class, args);
+	}
+}
---
+++ paolopro/src/test/java/it/Alfredo/Battaglieri/PaoloproApplicationTests.java
@@ -0,0 +1,16 @@
+package it.Alfredo.Battaglieri;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaoloproApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
